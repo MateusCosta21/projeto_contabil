aab536a9cd62dac186f72d2ce3e0ad541a50217e
mateuscosta <joao.santos@fabricainfo.com>
2023-04-06 16:18:11 -0300

    Ajuste logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -563,7 +563,35 @@ h1, .h1 {
             </nav>
         </div>
         <div class='dashboard-app'>
-            <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
+            <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+    
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+    
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+                    @unless (Auth::check())
+                        <li class="nav-item">
+                        </li>
+                    @endunless
+                </ul>
+            </header>
             <div class='dashboard-content'>
                 <div class='container'>
                     <div class='card'>
