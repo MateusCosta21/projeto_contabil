b2dae865f3d1727ff1fd7397cca860611b2ac5c5
mateuscosta <joao.santos@fabricainfo.com>
2023-04-11 12:36:26 -0300

    Tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,82 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="container-fluid pt-4 px-4">
+    <div class="row d-flex justify-content-center">
+        <div class="col-sm-6 col-xl-3">           
+             <div class="card bg-primary">
+              <div class="card-header ">
+                <div class="d-flex justify-content-center">
+                  <i class="fas fa-cogs fa-3x text-white"></i>
                 </div>
+                <div class="d-flex justify-content-center">
+                  <div class="huge text-white">30</div>
+                </div>
+                <div class="text-center text-white">Em processamento</div>
+              </div>
+              <a href='#em-processamento'>
+                <div class="card-footer text-center">
+                  <span style="color:#fff;">Ver detalhes</span>
+                  <i style="color:#fff;" class="fas fa-arrow-circle-right"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card bg-secondary">
+              <div class="card-header">
+                <div class="d-flex justify-content-center">
+                  <i class="fa fa-motorcycle fa-3x text-white"></i>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <div class="huge text-white">30</div>
+                </div>
+                <div class="text-center text-white">Em rota</div>
+              </div>
+              <a href='#em-processamento'>
+                <div class="card-footer text-center">
+                    <span style="color:#fff;">Ver detalhes</span>
+                    <i style="color:#fff;" class="fas fa-arrow-circle-right"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card bg-warning">
+              <div class="card-header">
+                <div class="d-flex justify-content-center">
+                  <i class="fa fa-building fa-3x text-white"></i>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <div class="huge text-white">30</div>
+                </div>
+                <div class="text-center text-white">No Condomínio/Cliente</div>
+              </div>
+              <a href='#em-processamento'>
+                <div class="card-footer text-center">
+                    <span style="color:#fff;">Ver detalhes</span>
+                    <i style="color:#fff;" class="fas fa-arrow-circle-right"></i>
+                </div>
+              </a>
+            </div>
+        </div>
+        <div class="col-sm-6 col-xl-3">
+            <div class="card bg-danger">
+              <div class="card-header">
+                <div class="d-flex justify-content-center">
+                  <i class="fa fa-hourglass fa-3x text-white"></i>
+                </div>
+                <div class="d-flex justify-content-center">
+                  <div class="huge text-white">30</div>
+                </div>
+                <div class="text-center text-white">Fora do prazo</div>
+              </div>
+              <a href='#em-processamento'>
+                <div class="card-footer text-center">
+                    <span style="color:#fff;">Ver detalhes</span>
+                    <i style="color:#fff;" class="fas fa-arrow-circle-right"></i>
+                </div>
+              </a>
             </div>
         </div>
     </div>
